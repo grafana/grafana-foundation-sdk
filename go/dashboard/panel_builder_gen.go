4a5d2d4574a5153073faefb15d1deb279b4fec4e
Kévin Gomez <kevin.gomez@grafana.com>
2024-04-09 12:30:55 +0200

    Automated release

--- go/dashboard/panel_builder_gen.go
+++ go/dashboard/panel_builder_gen.go
@@ -294,13 +294,6 @@ func (builder *PanelBuilder) NoValue(noValue string) *PanelBuilder {
 	return builder
 }
 
-// Defaults are the options applied to all fields.
-func (builder *PanelBuilder) Defaults(defaults FieldConfig) *PanelBuilder {
-	builder.internal.FieldConfig.Defaults = defaults
-
-	return builder
-}
-
 // Overrides are the options applied to specific fields overriding the defaults.
 func (builder *PanelBuilder) WithOverride(matcher MatcherConfig, properties []DynamicConfigValue) *PanelBuilder {
 	builder.internal.FieldConfig.Overrides = append(builder.internal.FieldConfig.Overrides, struct {
