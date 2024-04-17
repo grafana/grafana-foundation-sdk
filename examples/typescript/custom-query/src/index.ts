87c3243704795857bb9919e93fbb707220a24a2c
Kévin Gomez <kevin.gomez@grafana.com>
2024-04-17 16:57:45 +0200

    Add Typescript example for custom queries

---
+++ examples/typescript/custom-query/src/index.ts
@@ -0,0 +1,15 @@
+import { DashboardBuilder } from '@grafana/grafana-foundation-sdk/dashboard';
+import { PanelBuilder as TimeSeriesBuilder } from "@grafana/grafana-foundation-sdk/timeseries";
+import { CustomQueryBuilder } from "./customQuery";
+
+const builder = new DashboardBuilder('[Example] Custom query')
+    .uid('example-custom-query')
+    .withPanel(
+        new TimeSeriesBuilder()
+            .title('Sample panel')
+            .withTarget(
+                new CustomQueryBuilder("query here")
+            )
+    );
+
+console.log(JSON.stringify(builder.build(), null, 2));
