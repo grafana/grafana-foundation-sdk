9459ba8d78bf43f56811b7d04b4692e6ee64696b
Kévin Gomez <kevin.gomez@grafana.com>
2024-04-17 17:34:31 +0200

    Add the "Grafana Agent Overview" dashboard as a Typescript example

---
+++ examples/typescript/grafana-agent-overview/src/discovery.ts
@@ -0,0 +1,26 @@
+import * as timeseries from "@grafana/grafana-foundation-sdk/timeseries";
+import {defaultTimeseries, prometheusQuery} from "./common";
+
+export const targetSyncTimeseries = (): timeseries.PanelBuilder => {
+    return defaultTimeseries()
+        .title("Target Sync")
+        .description("Actual interval to sync the scrape pool.")
+        .datasource({uid: "$prometheus_datasource"})
+        .unit("s")
+        .withTarget(prometheusQuery(
+            'sum(rate(prometheus_target_sync_length_seconds_sum{job=~"$job", instance=~"$instance"}[$__rate_interval])) by (instance, scrape_job)',
+            "{{instance}}/{{scrape_job}}"
+        ));
+};
+
+export const targetsTimeseries = (): timeseries.PanelBuilder => {
+    return defaultTimeseries()
+        .title("Targets")
+        .description("Discovered targets by prometheus service discovery.")
+        .datasource({uid: "$prometheus_datasource"})
+        .unit("short")
+        .withTarget(prometheusQuery(
+            'sum by (instance) (prometheus_sd_discovered_targets{job=~"$job", instance=~"$instance"})',
+            "{{instance}}"
+        ));
+};
