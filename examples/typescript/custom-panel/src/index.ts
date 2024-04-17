5d9a01663f9b0b295d9b173c6f42ca3e2f40e722
Kévin Gomez <kevin.gomez@grafana.com>
2024-04-17 16:54:56 +0200

    Add Typescript example for custom panels

---
+++ examples/typescript/custom-panel/src/index.ts
@@ -0,0 +1,12 @@
+import { DashboardBuilder } from '@grafana/grafana-foundation-sdk/dashboard';
+import { CustomPanelBuilder } from "./customPanel";
+
+const builder = new DashboardBuilder('[Example] Custom panel')
+    .uid('example-custom-panel')
+    .withPanel(
+        new CustomPanelBuilder()
+            .title('Sample custom panel')
+            .makeBeautiful()
+    );
+
+console.log(JSON.stringify(builder.build(), null, 2));
