f29decbe45742152072b5172584e85572084308d
Kévin Gomez <kevin.gomez@grafana.com>
2024-04-17 17:34:31 +0200

    Add the "Grafana Agent Overview" dashboard as a Typescript example

---
+++ examples/typescript/grafana-agent-overview/src/index.ts
@@ -0,0 +1,89 @@
+import {
+    DashboardBuilder,
+    DashboardCursorSync,
+    DashboardLinkBuilder,
+    DashboardLinkType,
+    DatasourceVariableBuilder,
+    QueryVariableBuilder, RowBuilder,
+    TimePickerBuilder,
+    VariableRefresh,
+    VariableSort
+} from '@grafana/grafana-foundation-sdk/dashboard';
+import {runningInstancesTable} from "./overview";
+import {targetsTimeseries, targetSyncTimeseries} from "./discovery";
+import {appendedSamplesTimeseries, avgScrapeIntervalDurationTimeseries, scrapeFailuresTimeseries} from "./retrieval";
+
+const builder = new DashboardBuilder('[Example] Grafana Agent Overview')
+    .uid('example-integration-agent')
+    .tags(["generated", "grafana-foundation-sdk", "grafana-agent-integration"])
+    .editable()
+    .tooltip(DashboardCursorSync.Off)
+    .refresh("30s")
+    .time({ from: "now-30m", to: "now"})
+    .timezone("browser")
+    .timepicker(
+        new TimePickerBuilder()
+            .refresh_intervals(["5s", "10s", "30s", "1m", "5m", "15m", "30m", "1h", "2h", "1d"])
+            .time_options(["5m", "15m", "1h", "6h", "12h", "24h", "2d", "7d", "30d"]),
+    )
+    // Links to other agent-related dashboards
+    .link(
+        new DashboardLinkBuilder("Grafana Agent Dashboards")
+            .type(DashboardLinkType.Dashboards)
+            .icon("external link")
+            .tags(["grafana-agent-integration"])
+            .includeVars(true)
+            .keepTime(true)
+    )
+    // "Data source" variable
+    .withVariable(
+        new DatasourceVariableBuilder("prometheus_datasource")
+            .label("Data source")
+            .type("prometheus")
+            .regex("(?!grafanacloud-usage|grafanacloud-ml-metrics).+")
+            .multi(false)
+    )
+    // "Job" variable
+    .withVariable(
+        new QueryVariableBuilder("job")
+            .label("Job")
+            .query("label_values(agent_build_info, job)")
+            .datasource({uid: "$prometheus_datasource"})
+            .current({
+                selected: true,
+                text: "All",
+                value: "$__all",
+            })
+            .refresh(VariableRefresh.OnTimeRangeChanged)
+            .sort(VariableSort.AlphabeticalAsc)
+            .multi(true)
+            .includeAll(true)
+    )
+    // "Instance" variable
+    .withVariable(
+        new QueryVariableBuilder("instance")
+            .label("Instance")
+            .query("label_values(agent_build_info{job=~\"$job\"}, instance)")
+            .datasource({uid: "$prometheus_datasource"})
+            .current({
+                selected: true,
+                text: "All",
+                value: "$__all",
+            })
+            .refresh(VariableRefresh.OnTimeRangeChanged)
+            .sort(VariableSort.AlphabeticalAsc)
+            .multi(true)
+            .includeAll(true)
+    )
+    // Panels
+    .withRow(new RowBuilder("Overview"))
+    .withPanel(runningInstancesTable())
+    .withRow(new RowBuilder("Prometheus Discovery"))
+    .withPanel(targetSyncTimeseries())
+    .withPanel(targetsTimeseries())
+    .withRow(new RowBuilder("Prometheus Retrieval"))
+    .withPanel(avgScrapeIntervalDurationTimeseries())
+    .withPanel(scrapeFailuresTimeseries())
+    .withPanel(appendedSamplesTimeseries());
+
+console.log(JSON.stringify(builder.build(), null, 2));
---
+++ examples/typescript/grafana-agent-overview/src/overview.ts
@@ -0,0 +1,47 @@
+import * as common from '@grafana/grafana-foundation-sdk/common';
+import * as table from "@grafana/grafana-foundation-sdk/table";
+import {tablePrometheusQuery} from "./common";
+
+export const runningInstancesTable = (): table.PanelBuilder => {
+    return new table.PanelBuilder()
+        .title("Running Instances")
+        .description("General statistics of running grafana agent instances.")
+        .height(7)
+        .span(24)
+        .footer(
+            new common.TableFooterOptionsBuilder()
+                .countRows(false)
+                .reducer(["sum"])
+        )
+        .datasource({uid: "$prometheus_datasource"})
+        .withTarget(
+            tablePrometheusQuery('count by (instance, version) (agent_build_info{job=~"$job", instance=~"$instance"})', "A"),
+        )
+        .withTarget(
+            tablePrometheusQuery('max by (instance) (time() - process_start_time_seconds{job=~"$job", instance=~"$instance"})', "B"),
+        )
+        // Transformations
+        .withTransformation({
+            id: "merge",
+            options: {}
+        })
+        .withTransformation({
+            id: "organize",
+            options: {
+                excludeByName: {
+                    "Time": true,
+                    "Value #A": true,
+                },
+                "renameByName": {
+                    "Value #B": "Uptime",
+                },
+            },
+        })
+        // Overrides
+        .withOverride({
+            matcher: {id: "byName", options: "Value #B"},
+            properties: [
+                {id: "unit", value: "s"},
+            ]
+        });
+};
