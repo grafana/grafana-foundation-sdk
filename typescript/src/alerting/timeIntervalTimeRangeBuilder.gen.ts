1cd6fd5798be1750f4747d647f0c1e67877d7f3e
Duologic <jeroen@simplistic.be>
2024-06-04 13:49:17 +0200

    Automated release

---
+++ typescript/src/alerting/timeIntervalTimeRangeBuilder.gen.ts
@@ -0,0 +1,26 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+import * as cog from '../cog';
+import * as alerting from '../alerting';
+
+export class TimeIntervalTimeRangeBuilder implements cog.Builder<alerting.TimeIntervalTimeRange> {
+    protected readonly internal: alerting.TimeIntervalTimeRange;
+
+    constructor() {
+        this.internal = alerting.defaultTimeIntervalTimeRange();
+    }
+
+    build(): alerting.TimeIntervalTimeRange {
+        return this.internal;
+    }
+
+    endTime(endTime: string): this {
+        this.internal.end_time = endTime;
+        return this;
+    }
+
+    startTime(startTime: string): this {
+        this.internal.start_time = startTime;
+        return this;
+    }
+}
