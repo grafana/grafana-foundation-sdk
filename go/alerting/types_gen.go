c994d766ad6a640a3fa0315690c2388ef58922fa
Duologic <jeroen@simplistic.be>
2024-06-04 13:49:07 +0200

    Automated release

--- go/alerting/types_gen.go
+++ go/alerting/types_gen.go
@@ -10,111 +10,6 @@ import (
 	cogvariants "github.com/grafana/grafana-foundation-sdk/go/cog/variants"
 )
 
-// Duration in seconds.
-type Duration int64
-
-type Json any
-
-type MatchRegexps map[string]string
-
-type MatchType int64
-
-type Matcher struct {
-	Name  *string    `json:"Name,omitempty"`
-	Type  *MatchType `json:"Type,omitempty"`
-	Value *string    `json:"Value,omitempty"`
-}
-
-// Matchers is a slice of Matchers that is sortable, implements Stringer, and
-// provides a Matches method to match a LabelSet against all Matchers in the
-// slice. Note that some users of Matchers might require it to be sorted.
-type Matchers []Matcher
-
-type NotificationTemplate struct {
-	Name       *string     `json:"name,omitempty"`
-	Provenance *Provenance `json:"provenance,omitempty"`
-	Template   *string     `json:"template,omitempty"`
-}
-
-// Matchers is a slice of Matchers that is sortable, implements Stringer, and
-// provides a Matches method to match a LabelSet against all Matchers in the
-// slice. Note that some users of Matchers might require it to be sorted.
-type ObjectMatchers = Matchers
-
-type Provenance string
-
-// RelativeTimeRange is the per query start and end time
-// for requests.
-type RelativeTimeRange struct {
-	// RelativeTimeRange is the per query start and end time
-	// for requests.
-	From *Duration `json:"from,omitempty"`
-	// RelativeTimeRange is the per query start and end time
-	// for requests.
-	To *Duration `json:"to,omitempty"`
-}
-
-// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
-// within the interval.
-type TimeInterval struct {
-	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
-	// within the interval.
-	DaysOfMonth []string `json:"days_of_month,omitempty"`
-	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
-	// within the interval.
-	Location *string `json:"location,omitempty"`
-	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
-	// within the interval.
-	Months []string `json:"months,omitempty"`
-	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
-	// within the interval.
-	Times []TimeRange `json:"times,omitempty"`
-	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
-	// within the interval.
-	Weekdays []string `json:"weekdays,omitempty"`
-	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
-	// within the interval.
-	Years []string `json:"years,omitempty"`
-}
-
-// Redefining this to avoid an import cycle
-type TimeRange struct {
-	// Redefining this to avoid an import cycle
-	From *time.Time `json:"from,omitempty"`
-	// Redefining this to avoid an import cycle
-	To *time.Time `json:"to,omitempty"`
-}
-
-type RuleGroup struct {
-	FolderUid *string `json:"folderUid,omitempty"`
-	// The interval, in seconds, at which all rules in the group are evaluated.
-	// If a group contains many rules, the rules are evaluated sequentially.
-	Interval *Duration `json:"interval,omitempty"`
-	Rules    []Rule    `json:"rules,omitempty"`
-	Title    *string   `json:"title,omitempty"`
-}
-
-type Rule struct {
-	Annotations  map[string]string `json:"annotations,omitempty"`
-	Condition    string            `json:"condition"`
-	Data         []Query           `json:"data"`
-	ExecErrState RuleExecErrState  `json:"execErrState"`
-	FolderUID    string            `json:"folderUID"`
-	// The amount of time, in seconds, for which the rule must be breached for the rule to be considered to be Firing.
-	// Before this time has elapsed, the rule is only considered to be Pending.
-	For         string            `json:"for"`
-	Id          *int64            `json:"id,omitempty"`
-	IsPaused    *bool             `json:"isPaused,omitempty"`
-	Labels      map[string]string `json:"labels,omitempty"`
-	NoDataState RuleNoDataState   `json:"noDataState"`
-	OrgID       int64             `json:"orgID"`
-	Provenance  *Provenance       `json:"provenance,omitempty"`
-	RuleGroup   string            `json:"ruleGroup"`
-	Title       string            `json:"title"`
-	Uid         *string           `json:"uid,omitempty"`
-	Updated     *time.Time        `json:"updated,omitempty"`
-}
-
 type Query struct {
 	DatasourceUid     *string               `json:"datasourceUid,omitempty"`
 	Model             cogvariants.Dataquery `json:"model,omitempty"`
@@ -169,6 +64,18 @@ func (resource *Query) UnmarshalJSON(raw []byte) error {
 	return nil
 }
 
+type RuleGroup struct {
+	FolderUid *string `json:"folderUid,omitempty"`
+	// The interval, in seconds, at which all rules in the group are evaluated.
+	// If a group contains many rules, the rules are evaluated sequentially.
+	Interval *Duration `json:"interval,omitempty"`
+	Rules    []Rule    `json:"rules,omitempty"`
+	Title    *string   `json:"title,omitempty"`
+}
+
+// Duration in seconds.
+type Duration int64
+
 // EmbeddedContactPoint is the contact point type that is used
 // by grafanas embedded alertmanager implementation.
 type ContactPoint struct {
@@ -190,6 +97,80 @@ type ContactPoint struct {
 	// EmbeddedContactPoint is the contact point type that is used
 	// by grafanas embedded alertmanager implementation.
 	Uid *string `json:"uid,omitempty"`
+}
+
+type Json any
+
+type MatchRegexps map[string]string
+
+type MatchType string
+
+const (
+	MatchTypeEqual         MatchType = "="
+	MatchTypeNotEqual      MatchType = "!="
+	MatchTypeEqualRegex    MatchType = "=~"
+	MatchTypeNotEqualRegex MatchType = "!~"
+)
+
+type Matcher struct {
+	Name  *string    `json:"Name,omitempty"`
+	Type  *MatchType `json:"Type,omitempty"`
+	Value *string    `json:"Value,omitempty"`
+}
+
+// Matchers is a slice of Matchers that is sortable, implements Stringer, and
+// provides a Matches method to match a LabelSet against all Matchers in the
+// slice. Note that some users of Matchers might require it to be sorted.
+type Matchers []Matcher
+
+type MuteTiming struct {
+	Name          *string        `json:"name,omitempty"`
+	TimeIntervals []TimeInterval `json:"time_intervals,omitempty"`
+}
+
+type NotificationTemplate struct {
+	Name       *string     `json:"name,omitempty"`
+	Provenance *Provenance `json:"provenance,omitempty"`
+	Template   *string     `json:"template,omitempty"`
+}
+
+// Matchers is a slice of Matchers that is sortable, implements Stringer, and
+// provides a Matches method to match a LabelSet against all Matchers in the
+// slice. Note that some users of Matchers might require it to be sorted.
+type ObjectMatchers = Matchers
+
+type Provenance string
+
+type Rule struct {
+	Annotations  map[string]string `json:"annotations,omitempty"`
+	Condition    string            `json:"condition"`
+	Data         []Query           `json:"data"`
+	ExecErrState RuleExecErrState  `json:"execErrState"`
+	FolderUID    string            `json:"folderUID"`
+	// The amount of time, in seconds, for which the rule must be breached for the rule to be considered to be Firing.
+	// Before this time has elapsed, the rule is only considered to be Pending.
+	For         string            `json:"for"`
+	Id          *int64            `json:"id,omitempty"`
+	IsPaused    *bool             `json:"isPaused,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty"`
+	NoDataState RuleNoDataState   `json:"noDataState"`
+	OrgID       int64             `json:"orgID"`
+	Provenance  *Provenance       `json:"provenance,omitempty"`
+	RuleGroup   string            `json:"ruleGroup"`
+	Title       string            `json:"title"`
+	Uid         *string           `json:"uid,omitempty"`
+	Updated     *time.Time        `json:"updated,omitempty"`
+}
+
+// RelativeTimeRange is the per query start and end time
+// for requests.
+type RelativeTimeRange struct {
+	// RelativeTimeRange is the per query start and end time
+	// for requests.
+	From *Duration `json:"from,omitempty"`
+	// RelativeTimeRange is the per query start and end time
+	// for requests.
+	To *Duration `json:"to,omitempty"`
 }
 
 // A Route is a node that contains definitions of how to handle alerts. This is modified
@@ -236,26 +217,36 @@ type NotificationPolicy struct {
 	Routes []NotificationPolicy `json:"routes,omitempty"`
 }
 
-type MuteTiming struct {
-	Name          *string        `json:"name,omitempty"`
-	TimeIntervals []TimeInterval `json:"time_intervals,omitempty"`
+// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
+// within the interval.
+type TimeInterval struct {
+	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
+	// within the interval.
+	DaysOfMonth []string `json:"days_of_month,omitempty"`
+	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
+	// within the interval.
+	Location *string `json:"location,omitempty"`
+	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
+	// within the interval.
+	Months []string `json:"months,omitempty"`
+	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
+	// within the interval.
+	Times []TimeRange `json:"times,omitempty"`
+	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
+	// within the interval.
+	Weekdays []string `json:"weekdays,omitempty"`
+	// TimeInterval describes intervals of time. ContainsTime will tell you if a golang time is contained
+	// within the interval.
+	Years []string `json:"years,omitempty"`
 }
 
-type RuleExecErrState string
-
-const (
-	RuleExecErrStateOK       RuleExecErrState = "OK"
-	RuleExecErrStateAlerting RuleExecErrState = "Alerting"
-	RuleExecErrStateError    RuleExecErrState = "Error"
-)
-
-type RuleNoDataState string
-
-const (
-	RuleNoDataStateAlerting RuleNoDataState = "Alerting"
-	RuleNoDataStateNoData   RuleNoDataState = "NoData"
-	RuleNoDataStateOK       RuleNoDataState = "OK"
-)
+// Redefining this to avoid an import cycle
+type TimeRange struct {
+	// Redefining this to avoid an import cycle
+	From *time.Time `json:"from,omitempty"`
+	// Redefining this to avoid an import cycle
+	To *time.Time `json:"to,omitempty"`
+}
 
 type ContactPointType string
 
@@ -278,4 +269,20 @@ const (
 	ContactPointTypeVictorops    ContactPointType = " victorops"
 	ContactPointTypeWebhook      ContactPointType = " webhook"
 	ContactPointTypeWecom        ContactPointType = " wecom"
+)
+
+type RuleExecErrState string
+
+const (
+	RuleExecErrStateOK       RuleExecErrState = "OK"
+	RuleExecErrStateAlerting RuleExecErrState = "Alerting"
+	RuleExecErrStateError    RuleExecErrState = "Error"
+)
+
+type RuleNoDataState string
+
+const (
+	RuleNoDataStateAlerting RuleNoDataState = "Alerting"
+	RuleNoDataStateNoData   RuleNoDataState = "NoData"
+	RuleNoDataStateOK       RuleNoDataState = "OK"
 )
--- python/README.md
+++ python/README.md
@@ -9,7 +9,7 @@ A set of tools, types and *builder libraries* for building and manipulating Graf
 ## Installing
 
 ```shell
-python3 -m pip install 'grafana_foundation_sdk==1717160612!10.3.0'
+python3 -m pip install 'grafana_foundation_sdk==1717501740!10.3.0'
 ```
 
 ## Example usage
