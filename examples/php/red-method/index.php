1401c216d57b233c41dee7903559734d5659b84f
Kévin Gomez <kevin.gomez@grafana.com>
2024-06-25 23:30:36 +0200

    Add a PHP example of RED-style dashboard

---
+++ examples/php/red-method/index.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Red;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$redDashboard = Red\Dashboard::generate(
+    title: 'RED method',
+    serviceIDs: ['sample-service', 'payments', 'front-gateway'],
+);
+
+echo json_encode($redDashboard, JSON_PRETTY_PRINT).PHP_EOL;
