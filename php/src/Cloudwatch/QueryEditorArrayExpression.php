76ca231688902600e1f69f97ba83caf92ba2d898
Kévin Gomez <kevin.gomez@grafana.com>
2024-09-12 12:16:21 +0200

    Automated release

--- php/src/Cloudwatch/QueryEditorArrayExpression.php
+++ php/src/Cloudwatch/QueryEditorArrayExpression.php
@@ -34,20 +34,20 @@ class QueryEditorArrayExpression implements \JsonSerializable
         \assert(is_array($input), 'expected disjunction value to be an array');
     
         switch ($input["type"]) {
-        case "operator":
-            return QueryEditorOperatorExpression::fromArray($input);
         case "and":
             return QueryEditorArrayExpression::fromArray($input);
-        case "or":
-            return QueryEditorArrayExpression::fromArray($input);
-        case "property":
-            return QueryEditorPropertyExpression::fromArray($input);
-        case "groupBy":
-            return QueryEditorGroupByExpression::fromArray($input);
         case "function":
             return QueryEditorFunctionExpression::fromArray($input);
         case "functionParameter":
             return QueryEditorFunctionParameterExpression::fromArray($input);
+        case "groupBy":
+            return QueryEditorGroupByExpression::fromArray($input);
+        case "operator":
+            return QueryEditorOperatorExpression::fromArray($input);
+        case "or":
+            return QueryEditorArrayExpression::fromArray($input);
+        case "property":
+            return QueryEditorPropertyExpression::fromArray($input);
         default:
             throw new \ValueError('can not parse disjunction from array');
     }
