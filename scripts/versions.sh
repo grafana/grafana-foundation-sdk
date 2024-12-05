052b232dca551360326384472134d73ac3693c3e
Kévin Gomez <kevin.gomez@grafana.com>
2024-12-05 23:34:56 +0100

    Build the SDK for grafana v11.4.x

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,4 +2,4 @@
 
 COG_VERSION="v0.0.x" # hardcoded for now
 
-ALL_GRAFANA_VERSIONS="next;v11.3.x;v11.2.x;v11.1.x;v11.0.x;v10.4.x;v10.3.x;v10.2.x;v10.1.x"
+ALL_GRAFANA_VERSIONS="next;v11.4.x;v11.3.x;v11.2.x;v11.1.x;v11.0.x;v10.4.x;v10.3.x;v10.2.x;v10.1.x"
