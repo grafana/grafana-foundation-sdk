681af27fbfee867660eadd700ae8f2fc13b40844
Kévin Gomez <kevin.gomez@grafana.com>
2026-02-13 13:19:24 +0100

    Simplify the SDK Generate diff workflow

---
+++ scripts/release-preview-or-main.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump | gzip`
+set -o pipefail
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${__dir}/libs/git.sh"
+
+release_branch_exists=$(git_has_branch "." "release-preview")
+if [ "$release_branch_exists" != "0" ]; then
+  echo "main"
+else
+  echo 'release-preview'
+fi
