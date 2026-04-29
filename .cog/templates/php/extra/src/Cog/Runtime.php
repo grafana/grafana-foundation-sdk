02589f33f95f149aa1c152101c647b1a53080f70
Haris Rozajac <58232930+harisrozajac@users.noreply.github.com>
2026-04-29 05:33:11 -0600

    Add dashboard v2 stable resource (#1191)
    * add transforms
    
    * fix duplicate method implementations
    
    * Generate ControlSourceRef as a real struct
    
    * add compose fields
    
    * Update templates
    
    * Update builder configuration
    
    * Update cog version
    
    * Rename dataquery schema labels to avoid conflict with DataQueryKind one
    
    * Missing panel check
    
    * Remove dataquery datasource field from legacy schemas
    
    * Fix PHP
    
    * More PHP fixes
    
    * Missing v2 checks
    
    * Update cog to v0.1.10
    
    * Update cog
    
    ---------
    
    Co-authored-by: spinillos <selenepinillos@gmail.com>

--- .cog/templates/php/extra/src/Cog/Runtime.php
+++ .cog/templates/php/extra/src/Cog/Runtime.php
@@ -68,7 +68,7 @@ final class Runtime
     public function dataqueryFromArray(array $data, string $dataqueryTypeHint): Dataquery
     {
         // Dataqueries might reference the datasource to use, and its type. Let's use that.
-        if (empty($dataqueryTypeHint) && !empty($data['datasource']) && !empty($data['datasource']['type'])) {
+        if (empty($dataqueryTypeHint) && !empty($data['datasource']) && is_string($data['datasource']['type']) && $data['datasource']['type'] !== '') {
             $dataqueryTypeHint = $data['datasource']['type'];
         }
 
@@ -97,7 +97,7 @@ final class Runtime
     }
 
     {{ $panelArgument := "\\Grafana\\Foundation\\Dashboard\\Panel" }}
-    {{- if objectExists "dashboardv2beta1" "VizConfigKind" }}
+    {{- if or (objectExists "dashboardv2beta1" "VizConfigKind") (objectExists "dashboardv2" "VizConfigKind") }}
     {{- $panelArgument = "mixed" }}
     {{- end }}
 
@@ -133,8 +133,17 @@ final class Runtime
         return $convert($dataquery);
     }
 
-    {{- if objectExists "dashboardv2beta1" "DataQueryKind" }}
-    public function convertDataQueryKindToCode(\Grafana\Foundation\Dashboardv2beta1\DataQueryKind $dataquery, string $group): string 
+    {{- $typeHint := "" -}}
+    {{- if objectExists "dashboardv2beta1" "DataQueryKind" -}}
+      {{- $typeHint = "\\Grafana\\Foundation\\Dashboardv2beta1\\DataQueryKind" -}}
+    {{- end -}}
+    {{- if objectExists "dashboardv2" "DataQueryKind" -}}
+      {{- if ne $typeHint "" -}}{{- $typeHint = print $typeHint "|" -}}{{- end -}}
+      {{- $typeHint = print $typeHint "\\Grafana\\Foundation\\Dashboardv2\\DataQueryKind" -}}
+    {{- end -}}
+
+    {{- if ne $typeHint "" }}
+    public function convertDataQueryKindToCode({{ $typeHint }} $dataquery, string $group): string
     {
     	if (!isset($this->dataqueryVariants[$group])) {
             return '/* could not convert DataQueryKind to PHP */';
